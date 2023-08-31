609a7feea2fe3e7868504fc3d2e64c810065aba6
Umesh Dahal <umeshdahal.np@gmail.com>
2023-08-31 21:15:09 +0545

    Home Page

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
+
+// https://freevector-images.s3.amazonaws.com/uploads/vector/preview/36682/36682.png
---
+++ src/contexts/ProductContext.js
@@ -0,0 +1,21 @@
+import React, {createContext, useState, useEffect} from 'react'
+
+
+export const ProductContext = createContext();
+
+export const ProductProvider = ({children}) => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://dummyjson.com/products");
+      const data = await response.json();
+      setProducts(data);
+    };
+    fetchProducts();
+  }, [])
+  return (
+    <ProductContext.Provider value={{products}}>{children}</ProductContext.Provider>
+  )
+};
+
+export default ProductProvider;
